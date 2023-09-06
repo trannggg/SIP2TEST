3eed001e8fde20f943eb389e7fed54b4d507dc01
NguyenNgocAnh20205228 <anhtanul1@gmail.com>
2023-09-06 14:02:26 +0700

    change server

--- server.php
+++ server.php
@@ -9,7 +9,7 @@ ini_set('default_socket_timeout', -1);
 // Tạo socket
 $socket = socket_create(AF_INET, SOCK_STREAM, 0);
 socket_bind($socket, '127.0.0.1', 12346);
-socket_listen($socket);
+socket_listen($socket,5);
 echo "Server is listening on 127.0.0.1:12346\n";
 
 // Chấp nhận kết nối
@@ -17,35 +17,53 @@ $client_socket = socket_accept($socket);
 echo "Client connected\n";
 
 while (true) {
-    // Nhận dữ liệu từ client
-    $data = socket_read($client_socket, 1024);
+    try {
+        // Nhận dữ liệu từ client
+        $data = @socket_read($client_socket, 1024);
 
-    if ($data === false) {
-        echo "No data received\n";
-    } else {
-        echo "Received data: $data\n";
-        // Tách dữ liệu và checksum từ chuỗi
-        $msgToChecksumFull=explode('AZ',$data);
-        $msgToChecksum=$msgToChecksumFull[0]."AZ";
-        // Tính checksum
-        $checksum=applyChecksum($msgToChecksum);
-        if (strcmp($checksum,$data)){
-            $loginUserId = isset($loginFullUserID[1]) ? $loginFullUserID[1] : '';
-            // Xử lý dữ liệu
-            $data1=getData($data);
-            print_r($data1);
-            saveToJson($data1);
-            // Phản hồi tới client
-            list($data, $checksum1) = explode("AY", $data);
-            $response = "Server received: $data";
-            socket_write($client_socket, $data, strlen($response));
 
-            // In dữ liệu nhận được
-            echo "Response sent to client: $response\n";
-        }else{
-            echo "Error Checksum ";
+        if ($data === false) {
+            $socket_error = socket_last_error($client_socket);
+
+            // Kiểm tra nếu kết nối đã đóng
+            if ($socket_error === 104 || $socket_error === 0) {
+                echo "Client disconnected\n";
+                break; // Thoát khỏi vòng lặp khi kết nối đã đóng
+            } else {
+                // Xử lý lỗi khác nếu có
+                echo "Socket error: " . socket_strerror($socket_error) . "\n";
+                break;
+            }
+        } elseif (empty($data)) {
+
+            break;
+        } else {
+            echo "Received data: $data\n";
+            // Tách dữ liệu và checksum từ chuỗi
+            $msgToChecksumFull = explode('AZ', $data);
+            $msgToChecksum = $msgToChecksumFull[0] . "AZ";
+            // Tính checksum
+            $checksum = applyChecksum($msgToChecksum);
+            if (strcmp($checksum, $data)) {
+                $loginUserId = isset($loginFullUserID[1]) ? $loginFullUserID[1] : '';
+                // Xử lý dữ liệu
+                $data1 = getData($data);
+                print_r($data1);
+                saveToJson($data1);
+                // Phản hồi tới client
+                list($data, $checksum1) = explode("AY", $data);
+                $response = "Server received: $data";
+                socket_write($client_socket, $data, strlen($response));
+
+                // In dữ liệu nhận được
+                echo "Response sent to client: $response\n";
+            } else {
+                echo "Error Checksum ";
+            }
         }
-
+    } catch (Exception $e) {
+        // Xử lý lỗi và đóng kết nối
+        echo "Error: " . $e->getMessage() . "\n";
     }
 }
 
